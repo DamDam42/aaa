c41c8b885a2e6c2407331a074de2b8234d0dc7ea
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-10 02:41:00 +0800

    A

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -1,8 +1,7 @@
 package com.heroku.java;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Map;
+import java.sql.PreparedStatement;
 
 import javax.sql.DataSource;
 
@@ -11,6 +10,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.heroku.java.model.customer;
 
 @SpringBootApplication
 @Controller
@@ -28,27 +31,27 @@ public class GettingStartedApplication {
     }
     
 
-    @GetMapping("/database")
-    String database(Map<String, Object> model) {
-        try (Connection connection = dataSource.getConnection()) {
-            final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+   
 
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-            final var output = new ArrayList<>();
-            while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
-            }
-
-            model.put("records", output);
-            return "database";
-
-        } catch (Throwable t) {
-            model.put("message", t.getMessage());
-            return "error";
+    @PostMapping("/customerRegister")
+    public String customerRegister(@ModelAttribute("customerRegister") customer customer) {
+        try (Connection connection = dataSource.getConnection()){
+            String sql = "INSERT INTO public.customer(custname, custpassword, custemail, custphonenum, custaddress) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, customer.getCustName());
+            statement.setString(2, customer.getCustPassword());
+            statement.setString(3, customer.getCustEmail());
+            statement.setString(4, customer.getCustPhoneNum());
+            statement.setString(5, customer.getCustAddress());
+            statement.executeUpdate();
+            System.out.println("Name: " + customer.getCustName());
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/index";
         }
-    }
+        return "redirect:/account";
+    } 
 
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
