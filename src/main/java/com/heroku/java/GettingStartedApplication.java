0d108498a59b5aaa8f3491996cdc01749ffd8cf2
DamDam42 <165986549+DamDam42@users.noreply.github.com>
2024-06-10 15:25:09 +0800

    Update GettingStartedApplication.java

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -17,7 +17,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @SpringBootApplication
 public class GettingStartedApplication {
+private final Datasource dataSource;
+
+    @Autowired
+    public class GettingStartedApplication(Datasource dataSource){
+    this.dataSource = dataSource;
+}
+
+@GetMapping("/")
+    public String index(){
+    return "index";
+}
     
+     @GetMapping("/database")
+    public String database(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+            final var statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+
+            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
+            final var output = new ArrayList<String>();
+            while (resultSet.next()) {
+                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+            }
+
+            model.put("records", output);
+            return "database";
+        } catch (Throwable t) {
+            model.put("message", t.getMessage());
+            return "error";
+        }
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
     }
