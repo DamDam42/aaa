07c4d31d7af89ebe808bb76bc9651e45fdcd05bd
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-10 16:41:01 +0800

    a

--- src/main/java/com/heroku/java/controller/CustomerController.java
+++ src/main/java/com/heroku/java/controller/CustomerController.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -23,10 +22,7 @@ public class CustomerController {
         this.dataSource=dataSource;
     }
    
-    @GetMapping("/customerRegister")
-    public String customerRegister() {
-        return "account/customerRegister";
-    }
+    
 
     
     @PostMapping("/customerRegister")
