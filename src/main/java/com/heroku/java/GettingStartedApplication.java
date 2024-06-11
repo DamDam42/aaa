6a0745712e49162799815fcdaf462e5d3188b52d
DamDam42 <165986549+DamDam42@users.noreply.github.com>
2024-06-11 18:19:06 +0800

    Update GettingStartedApplication.java

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,30 +10,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-
-
-
-@Controller
 @SpringBootApplication
+@Controller
 public class GettingStartedApplication {
-private final DataSource dataSource;
+    private final DataSource dataSource;
 
     @Autowired
-    public GettingStartedApplication(DataSource dataSource){
-    this.dataSource = dataSource;
+    public GettingStartedApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+// for guest
+@GetMapping ("/viewticket")
+public String viewticket(){
+    return "account/createBooking";
 }
-
-@GetMapping("/")
-    public String index(){
-    return "index";
-}
-
-
-    
+   
 
     @GetMapping("/database")
     String database(Map<String, Object> model) {
@@ -43,22 +37,21 @@ private final DataSource dataSource;
             statement.executeUpdate("INSERT INTO ticks VALUES (now())");
 
             final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-            final var output = new ArrayList<String>();
+            final var output = new ArrayList<>();
             while (resultSet.next()) {
                 output.add("Read from DB: " + resultSet.getTimestamp("tick"));
             }
 
             model.put("records", output);
             return "database";
+
         } catch (Throwable t) {
             model.put("message", t.getMessage());
             return "error";
         }
     }
+
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
-    } 
+    }
 }
-
-
-
